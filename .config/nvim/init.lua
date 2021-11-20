24439b19e30db588e2528f3f0d5fb581f275c34b
Ruslan V <vruslan@gmail.com>
2021-11-20 19:38:44 +0000

    using packer as plugin manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,3 +26,59 @@ vim.o.termguicolors=true
 vim.wo.colorcolumn='90'
 
 vim.g.mapleader=' '
+vim.g.completeopt='menu,menuone,noselect'
+
+
+
+
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.cmd [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]]
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'morhetz/gruvbox'
+  use 'joshdick/onedark.vim' -- Theme inspired by Atom
+
+  use 'tpope/vim-commentary'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'nvim-lua/completion-nvim'
+  use 'tjdevries/nlua.nvim'
+end)
+
+vim.cmd [[colorscheme gruvbox]]
+
+
+
+-- To get builtin LSP running, do something like:
+-- NOTE: This replaces the calls where you would have before done `require('nvim_lsp').sumneko_lua.setup()`
+-- require('nlua.lsp.nvim').setup(require('lspconfig'), {
+--   on_attach = on_attach,
+
+--   -- Include globals you want to tell the LSP are real :)
+--   globals = {
+--     -- Colorbuddy
+--     "Color", "c", "Group", "g", "s",
+--   }
+-- })
