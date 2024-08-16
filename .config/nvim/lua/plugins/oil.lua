c3d2fe76c2959d342e527d176f311c53342f3c72
Ruslan V <vruslan@gmail.com>
2024-08-16 19:53:53 +0100

    some formatting and lazy setup

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,8 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
-	},
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
 }
