b48b93d2da2266179e64fe5972aec599ea2c469c
Ruslan V <vruslan@gmail.com>
2024-08-19 17:18:50 +0100

    bufferline keymaps and options

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,25 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    { "<S-h>", "<CMD>bprev<CR>", desc = "Buffer prev" },
-    { "<S-l>", "<CMD>bnext<CR>", desc = "Buffer next" },
-  },
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	version = "*",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{ "<S-h>", "<CMD>bprev<CR>", desc = "Buffer prev" },
+		{ "<S-l>", "<CMD>bnext<CR>", desc = "Buffer next" },
+		{ "<A-h>", "<CMD>BufferLineMovePrev<CR>", desc = "Buffer move left" },
+		{ "<A-l>", "<CMD>BufferLineMoveNext<CR>", desc = "Buffer move right" },
+	},
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			diagnostics_indicator = function(_, _, diag)
+				local ret = (diag.error and "󰅚 " .. diag.error .. " " or "")
+					.. (diag.warning and "󰀪 " .. diag.warning or "")
+				return vim.trim(ret)
+			end,
+		},
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+	end,
 }
