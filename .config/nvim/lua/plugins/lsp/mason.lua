2c0b28995561917cabaaa7a52720e894c4d63c2d
Ruslan V <vruslan@gmail.com>
2024-12-07 10:28:35 +0000

    disabling cssls

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,44 +1,44 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "lua_ls",
-        "omnisharp",
-        -- "csharp_ls",
-        "ts_ls",
-        -- "tsserver",
-        "angularls",
-        "html",
-        "cssls",
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls",
+				"omnisharp",
+				-- "csharp_ls",
+				"ts_ls",
+				-- "tsserver",
+				"angularls",
+				"html",
+				-- "cssls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "stylua",
-        "prettier",
-        -- "prettierd",
-        "eslint_d",
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				-- "prettierd",
+				"eslint_d",
+			},
+		})
+	end,
 }
