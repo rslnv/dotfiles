e08bee56a86c41da22d43e719ade3e0fb20ca35c
Ruslan V <vruslan@gmail.com>
2025-02-20 12:23:33 +0000

    more groups in which-key

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,21 +1,23 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "Buffer Local Keymaps",
-		},
-	},
-	config = function()
-		require("which-key").add({
-			{ "<leader>c", group = "Code" },
-			{ "<leader>d", group = "Debugger" },
-			{ "<leader>f", group = "Find" },
-			{ "<leader>h", group = "Hunk" },
-		})
-	end,
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps",
+    },
+  },
+  config = function()
+    require("which-key").add({
+      { "<leader>c", group = "Code" },
+      { "<leader>d", group = "Debugger" },
+      { "<leader>e", group = "Edit" },
+      { "<leader>f", group = "Find" },
+      { "<leader>h", group = "Hunk" },
+      { "<leader>x", group = "Execute" },
+    })
+  end,
 }
