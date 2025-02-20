8a28d04c4a0a287ee84fed8f7ec606ee997f3568
Ruslan V <vruslan@gmail.com>
2025-02-20 12:25:37 +0000

    clean-up

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,9 +7,6 @@ return {
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
 				config = function()
 					require("telescope").load_extension("fzf")
 				end,
@@ -21,6 +18,14 @@ return {
 				layout_config = {
 					vertical = { width = 0.8 },
 				},
+			},
+			-- pickers = {
+			-- 	find_files = {
+			-- 		theme = "ivy",
+			-- 	},
+			-- },
+			extensions = {
+				fzf = {},
 			},
 		},
 	},
