915d818af2ff87bf80a58bee83f8f21757cd6c93
Ruslan V <vruslan@gmail.com>
2025-04-04 17:20:45 +0100

    formatting

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,115 +1,105 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking",
-	group = vim.api.nvim_create_augroup("UserHighlightYank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking",
+  group = vim.api.nvim_create_augroup("UserHighlightYank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		--HACK: disable angular renaming capability due to duplicate rename popping up
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client and client.config.name == "angularls" then
-			client.server_capabilities.renameProvider = false
-		end
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    --HACK: disable angular renaming capability due to duplicate rename popping up
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client.config.name == "angularls" then
+      client.server_capabilities.renameProvider = false
+    end
 
-		local telescope_builtin = require("telescope.builtin")
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    local telescope_builtin = require("telescope.builtin")
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf, silent = true }
-		vim.keymap.set(
-			"n",
-			"gD",
-			vim.lsp.buf.declaration,
-			vim.tbl_extend("error", opts, { desc = "Jump to declaration" })
-		)
-		vim.keymap.set(
-			"n",
-			"gd",
-			telescope_builtin.lsp_definitions,
-			-- vim.lsp.buf.definition,
-			vim.tbl_extend("error", opts, { desc = "List definitions" })
-		)
-		vim.keymap.set(
-			"n",
-			"gt",
-			telescope_builtin.lsp_type_definitions,
-			-- vim.lsp.buf.type_definition,
-			vim.tbl_extend("error", opts, { desc = "List type definition" })
-		)
-		vim.keymap.set(
-			"n",
-			"gi",
-			telescope_builtin.lsp_implementations,
-			-- vim.lsp.buf.implementation,
-			vim.tbl_extend("error", opts, { desc = "List implementations" })
-		)
-		vim.keymap.set(
-			"n",
-			"gr",
-			telescope_builtin.lsp_references,
-			-- vim.lsp.buf.references,
-			vim.tbl_extend("error", opts, { desc = "List references" })
-		)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf, silent = true }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("error", opts, { desc = "Jump to declaration" }))
+    vim.keymap.set(
+      "n",
+      "gd",
+      telescope_builtin.lsp_definitions,
+      -- vim.lsp.buf.definition,
+      vim.tbl_extend("error", opts, { desc = "List definitions" })
+    )
+    vim.keymap.set(
+      "n",
+      "gt",
+      telescope_builtin.lsp_type_definitions,
+      -- vim.lsp.buf.type_definition,
+      vim.tbl_extend("error", opts, { desc = "List type definition" })
+    )
+    vim.keymap.set(
+      "n",
+      "gi",
+      telescope_builtin.lsp_implementations,
+      -- vim.lsp.buf.implementation,
+      vim.tbl_extend("error", opts, { desc = "List implementations" })
+    )
+    vim.keymap.set(
+      "n",
+      "gr",
+      telescope_builtin.lsp_references,
+      -- vim.lsp.buf.references,
+      vim.tbl_extend("error", opts, { desc = "List references" })
+    )
 
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("error", opts, { desc = "Symbol info" }))
-		vim.keymap.set(
-			"n",
-			"<C-k>",
-			vim.lsp.buf.signature_help,
-			vim.tbl_extend("error", opts, { desc = "Signature help" })
-		)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("error", opts, { desc = "Symbol info" }))
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, vim.tbl_extend("error", opts, { desc = "Signature help" }))
 
-		vim.keymap.set(
-			"n",
-			"<leader>wa",
-			vim.lsp.buf.add_workspace_folder,
-			vim.tbl_extend("error", opts, { desc = "Add workspace folder" })
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>wr",
-			vim.lsp.buf.remove_workspace_folder,
-			vim.tbl_extend("error", opts, { desc = "Remove workspace folder" })
-		)
-		vim.keymap.set("n", "<leader>wl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, vim.tbl_extend("error", opts, { desc = "List workspace folders" }))
+    vim.keymap.set(
+      "n",
+      "<leader>wa",
+      vim.lsp.buf.add_workspace_folder,
+      vim.tbl_extend("error", opts, { desc = "Add workspace folder" })
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>wr",
+      vim.lsp.buf.remove_workspace_folder,
+      vim.tbl_extend("error", opts, { desc = "Remove workspace folder" })
+    )
+    vim.keymap.set("n", "<leader>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, vim.tbl_extend("error", opts, { desc = "List workspace folders" }))
 
-		vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("error", opts, { desc = "Rename symbol" }))
-		vim.keymap.set(
-			{ "n", "v" },
-			"<leader>ca",
-			vim.lsp.buf.code_action,
-			vim.tbl_extend("error", opts, { desc = "Actions" })
-		)
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("error", opts, { desc = "Rename symbol" }))
+    vim.keymap.set(
+      { "n", "v" },
+      "<leader>ca",
+      vim.lsp.buf.code_action,
+      vim.tbl_extend("error", opts, { desc = "Actions" })
+    )
 
-		-- more tsserver specific code actions
-		-- https://www.lazyvim.org/extras/lang/typescript
-		-- https://www.reddit.com/r/neovim/comments/192jxlv/how_to_get_tsserver_auto_add_imports_on_save/
-		-- https://neovim.io/doc/user/lsp.html
-		-- if client and client.config.name == "tsserver" then
-		if client and client.config.name == "ts_ls" then
-			vim.keymap.set("n", "<leader>co", function()
-				vim.lsp.buf.code_action({ apply = true, context = { only = { "source.organizeImports" } } })
-			end, vim.tbl_extend("error", opts, { desc = "Organize imports" }))
+    -- more tsserver specific code actions
+    -- https://www.lazyvim.org/extras/lang/typescript
+    -- https://www.reddit.com/r/neovim/comments/192jxlv/how_to_get_tsserver_auto_add_imports_on_save/
+    -- https://neovim.io/doc/user/lsp.html
+    -- if client and client.config.name == "tsserver" then
+    if client and client.config.name == "ts_ls" then
+      vim.keymap.set("n", "<leader>co", function()
+        vim.lsp.buf.code_action({ apply = true, context = { only = { "source.organizeImports" } } })
+      end, vim.tbl_extend("error", opts, { desc = "Organize imports" }))
 
-			vim.keymap.set("n", "<leader>cm", function()
-				vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" } } })
-			end, vim.tbl_extend("error", opts, { desc = "Add missing imports" }))
+      vim.keymap.set("n", "<leader>cm", function()
+        vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" } } })
+      end, vim.tbl_extend("error", opts, { desc = "Add missing imports" }))
 
-			vim.keymap.set("n", "<leader>cu", function()
-				vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" } } })
-			end, vim.tbl_extend("error", opts, { desc = "Remove unused imports" }))
-		end
-	end,
+      vim.keymap.set("n", "<leader>cu", function()
+        vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" } } })
+      end, vim.tbl_extend("error", opts, { desc = "Remove unused imports" }))
+    end
+  end,
 })
 
 -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,54 +1,55 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"onsails/lspkind.nvim",
-		},
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			local lspkind = require("lspkind")
+      local lspkind = require("lspkind")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					-- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer", max_item_count = 5 },
-					{ name = "path" },
-				}),
-				formatting = {
-					format = lspkind.cmp_format({
-						-- mode = 'symbol',
-						maxwidth = 50,
-						ellipsis_char = "...",
-					}),
-				},
-			})
-		end,
-	},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer", max_item_count = 5 },
+          { name = "path" },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            -- mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+      })
+    end,
+  },
 }
