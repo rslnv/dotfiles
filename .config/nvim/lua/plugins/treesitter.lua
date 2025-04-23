ac34c901ff8e4197be197e9dd56a5d77e3240ff6
Ruslan V <vruslan@gmail.com>
2025-04-23 09:05:16 +0100

    treesitter config refactoring

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,13 +4,22 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-      ensure_installed = { "lua", "c", "c_sharp", "angular", "typescript", "javascript", "html", "css", "scss" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "vimdoc",
+      "lua",
+      "c",
+      "c_sharp",
+      "angular",
+      "typescript",
+      "javascript",
+      "html",
+      "css",
+      "scss",
+    },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
