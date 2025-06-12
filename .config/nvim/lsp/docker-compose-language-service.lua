beab88fd5a163650ad14faf80dc8e33182c79789
Ruslan V <vruslan@gmail.com>
2025-06-12 15:13:15 +0100

    adding docker compose lsp

---
+++ .config/nvim/lsp/docker-compose-language-service.lua
@@ -0,0 +1,8 @@
+-- Install with: npm i -g compose-language-service
+
+---@type vim.lsp.Config
+return {
+  -- cmd = { "docker-compose-langserver", "--stdio" },
+  -- filetypes = { "yaml.docker-compose" },
+  -- root_markers = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
+}
