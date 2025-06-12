3520907c5b8a65a451f7b1b71a45e5d8441a6e58
Ruslan V <vruslan@gmail.com>
2025-06-12 15:14:55 +0100

    conform options

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -12,10 +12,8 @@ return {
     },
   },
   opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
+    default_format_opts = { lsp_format = "fallback" },
+    format_on_save = { timeout_ms = 500 },
     formatters_by_ft = {
       lua = { "stylua" },
       javascript = { "prettier" },
